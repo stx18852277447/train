e3d1a6d07f6d1f4e078b7152b0e7d4f1b5522e85
stx18852277447` <1071852424@qq.com>
2023-11-20 15:31:27 +0800

    测试

--- member/src/main/java/top/stx/train/member/controller/TestController.java
+++ member/src/main/java/top/stx/train/member/controller/TestController.java
@@ -1,10 +1,10 @@
-package top.stx.train;
+package top.stx.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestContrroller {
+public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
