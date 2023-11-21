ac65863ae34f528e8a04c2bc3e212d3552bf1720
stx18852277447` <1071852424@qq.com>
2023-11-21 20:16:39 +0800

    制作 vue.ftl 模版

--- generator/src/main/java/top/stx/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/top/stx/train/generator/gen/ServerGenerator.java
@@ -20,6 +20,9 @@ public class ServerGenerator {
     static String servicePath = "[module]/src/main/java/top/stx/train/[module]/service/";
     static String serverPath = "[module]/src/main/java/top/stx/train/[module]/";
 
+    static String vuePath = "web/src/views/main/";
+    static boolean readOnly = false;
+
     static String pomPath = "generator/pom.xml";
 
     static {
@@ -77,6 +80,7 @@ public class ServerGenerator {
         param.put("tableNameCn", tableNameCn);
         param.put("fieldList", fieldList);
         param.put("typeSet", typeSet);
+        param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
 
         /*gen(Domain, param, "service", "service");
@@ -86,7 +90,8 @@ public class ServerGenerator {
 
         //gen(Domain, param, "req", "queryReq");
 
-        gen(Domain, param, "resp", "queryResp");
+        //gen(Domain, param, "resp", "queryResp");
+        genVue(do_main, param);
     }
 
     /**
@@ -119,5 +124,12 @@ public class ServerGenerator {
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
         return node.getText();
+    }
+    private static void genVue(String do_main, Map<String, Object> param) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig("vue.ftl");
+        new File(vuePath).mkdirs();
+        String fileName = vuePath + do_main + ".vue";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 }
