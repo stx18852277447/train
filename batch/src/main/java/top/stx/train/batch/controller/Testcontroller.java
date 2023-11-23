4a3b4dd8c055fc59b23c2d6bf2baa5c4dc46aa5d
stx18852277447` <1071852424@qq.com>
2023-11-23 13:33:32 +0800

    增加 batch 定时调度模块

---
+++ batch/src/main/java/top/stx/train/batch/controller/Testcontroller.java
@@ -0,0 +1,12 @@
+package top.stx.train.batch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testcontroller {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
