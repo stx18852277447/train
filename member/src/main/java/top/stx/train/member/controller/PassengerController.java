34806279fcce009ab00a1df04885349efd404eb7
stx18852277447` <1071852424@qq.com>
2023-11-26 17:30:35 +0800

    订票页面勾选乘客并显示购票列表

--- member/src/main/java/top/stx/train/member/controller/PassengerController.java
+++ member/src/main/java/top/stx/train/member/controller/PassengerController.java
@@ -1,5 +1,7 @@
 package top.stx.train.member.controller;
 
+import java.util.List;
+
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,21 +25,30 @@ import top.stx.train.member.service.PassengerService;
 public class PassengerController {
     @Resource
     private PassengerService passengerService;
+
     @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
         passengerService.save(req);
         return new CommonResp<>();
     }
 
     @GetMapping("/query-list")
-    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
         req.setMemberId(LoginMemberContext.getId());
         PageResp<PassengerQueryResp> list = passengerService.queryList(req);
         return new CommonResp<>(list);
     }
+
     @DeleteMapping("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable Long id) {
         passengerService.delete(id);
         return new CommonResp<>();
     }
+
+    @GetMapping("/query-mine")
+    public CommonResp<List<PassengerQueryResp>> queryMine() {
+        List<PassengerQueryResp> list = passengerService.queryMine();
+        return new CommonResp<>(list);
+    }
+
 }
--- member/src/main/java/top/stx/train/member/service/PassengerService.java
+++ member/src/main/java/top/stx/train/member/service/PassengerService.java
@@ -25,39 +25,40 @@ import top.stx.train.member.resp.PassengerQueryResp;
 public class PassengerService {
     @Resource
     private PassengerMapper passengerMapper;
-    public void save(PassengerSaveReq req){
+
+    public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
-        Passenger passenger = BeanUtil.copyProperties(req,Passenger.class);
-        if(ObjectUtil.isNull(passenger.getId())){
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        if (ObjectUtil.isNull(passenger.getId())) {
             passenger.setMemberId(LoginMemberContext.getId());
             passenger.setId(SnowUtil.getSnowflakeNextId());
             passenger.setCreateTime(now);
             passenger.setUpdateTime(now);
             passengerMapper.insert(passenger);
-        }else{
+        } else {
             passenger.setUpdateTime(now);
             passengerMapper.updateByPrimaryKey(passenger);
         }
 
     }
 
-    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
         PassengerExample passengerExample = new PassengerExample();
         passengerExample.setOrderByClause("id desc");
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
-        if(ObjectUtil.isNotNull(req.getMemberId())){
+        if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(
                 req.getMemberId()
             );
         }
-        log.info("查询页码：{}",req.getPage());
-        log.info("每页条数：{}",req.getSize());
-        PageHelper.startPage(req.getPage(),req.getSize());
-        List<Passenger> passengerList =  passengerMapper.selectByExample(passengerExample);
+        log.info("查询页码：{}", req.getPage());
+        log.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
         PageInfo<Passenger> pasgeInfo = new PageInfo<>(passengerList);
-        log.info("总行数：{}",pasgeInfo.getTotal());
-        log.info("总页数：{}",pasgeInfo.getPages());
-        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList,PassengerQueryResp.class);
+        log.info("总行数：{}", pasgeInfo.getTotal());
+        log.info("总页数：{}", pasgeInfo.getPages());
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
 
         PageResp<PassengerQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pasgeInfo.getTotal());
@@ -65,7 +66,16 @@ public class PassengerService {
         return pageResp;
     }
 
-    public void delete(Long id){
-        passengerMapper .deleteByPrimaryKey(id);
+    public void delete(Long id) {
+        passengerMapper.deleteByPrimaryKey(id);
+    }
+
+    public List<PassengerQueryResp> queryMine() {
+        PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("name asc");
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        criteria.andMemberIdEqualTo(LoginMemberContext.getId());
+        List<Passenger> list = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(list, PassengerQueryResp.class);
     }
 }
