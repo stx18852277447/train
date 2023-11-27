bce3b04482c93e42779676daad55eeec4fbff431
stx18852277447` <1071852424@qq.com>
2023-11-27 16:30:00 +0800

    配置 Feign + Nacos 支持应用名远程调用

--- business/src/main/java/top/stx/train/business/feign/MemberFeign.java
+++ business/src/main/java/top/stx/train/business/feign/MemberFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import top.stx.train.common.req.MemberTicketReq;
 import top.stx.train.common.resp.CommonResp;
 
-@FeignClient(name = "member", url = "http://127.0.0.1:8001")
+@FeignClient("member")
 public interface MemberFeign {
 
     @GetMapping("/member/feign/ticket/save")
